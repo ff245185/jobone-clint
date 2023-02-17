7ccdd287f6f498c494cccbdc33a4c9f6b968bcd4
unknown <ff245185@gmail.com>
2023-02-17 02:54:36 -0800

    git hash

---
+++ src/authprovider/Authprovider.js
@@ -0,0 +1,57 @@
+import React, { children, createContext, useEffect, useState } from "react";
+import React, { createContext } from 'react';
+import app from '../firebase/Firebase.config'
+import {getAuth,GoogleAuthProvider,createUserWithEmailAndPassword} from 'firebase/auth'
+export const AuthContext = createContext();
+const auth = getAuth(app)
+const Authprovider = ({}) => {
+    const [user,setUser] = useState(); 
+    const [loding, setLoding] = useState();
+    
+    const googleProvider = new GoogleAuthProvider();
+    const continewWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const createUserWithEmailAndPass = (email, password) => {
+		setLoding(true);
+		return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const logInWithEmailAndPassword = (email, password) => {
+		setLoading(true);
+		return signInWithEmailAndPassword(auth, email, password);}
+
+        const userLogOut = () => {
+            localStorage.clear();
+            return signOut(auth);
+        };
+        useEffect(() => {
+            const unSubscribes = onAuthStateChanged(auth, currentUser => {
+                setUser(currentUser);
+    
+               setLoding(false)
+            });
+            return () => {
+                unSubscribes();
+            };
+        }, []);
+        const authInfo = {
+          loding,
+            user,
+            createUserWithEmailAndPass,
+            userProfileUpdate,
+            logInWithEmailAndPassword,
+            userLogOut,
+           continewWithGoogle,
+        setLoding
+        };
+
+
+    return (
+        <div>
+     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+        </div>
+    );
+};
+
+export default Authprovider;
---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC1z93MtFKnd4-P6fpQ1ojcM7e3ehRIQqc",
+  authDomain: "aircnc-66d30.firebaseapp.com",
+  projectId: "aircnc-66d30",
+  storageBucket: "aircnc-66d30.appspot.com",
+  messagingSenderId: "94331615300",
+  appId: "1:94331615300:web:8b7b7b81930257778ba411"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
