6e9b5b7a090de9d1c1675f43c556c447ed560b11
unknown <ff245185@gmail.com>
2023-02-23 22:03:58 +0600

    firebase code change

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyC1z93MtFKnd4-P6fpQ1ojcM7e3ehRIQqc",
-  authDomain: "aircnc-66d30.firebaseapp.com",
-  projectId: "aircnc-66d30",
-  storageBucket: "aircnc-66d30.appspot.com",
-  messagingSenderId: "94331615300",
-  appId: "1:94331615300:web:8b7b7b81930257778ba411"
+  apiKey: "AIzaSyD-FR01fVdgdxtDP3rKux6wH56Fp9twdl4",
+  authDomain: "my-one-b5e43.firebaseapp.com",
+  projectId: "my-one-b5e43",
+  storageBucket: "my-one-b5e43.appspot.com",
+  messagingSenderId: "304733724699",
+  appId: "1:304733724699:web:80e22cf863dc204e02d8be",
+  measurementId: "G-WRZ3NBP6C4"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default app;
--- src/share/Navebar.js
+++ src/share/Navebar.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../authprovider/Authprovider';
 
 
 const Navebar = () => {
+
+  const { user, logOut } = useContext(AuthContext);
+ const handleLogout = () =>{
+         logOut()
+         .then(()=>{})
+ }
+
+
     return (
         <div className="navbar bg-base-100">
         <div className="flex-1">
@@ -19,7 +28,35 @@ const Navebar = () => {
               
             </li>
             <li><Link>Top-post</Link></li>
-            <li><Link to='/login'>Login</Link></li>
+            {/* <li><Link to='/login'>Login</Link></li> */}
+
+            {/* <li><Link to='/logout'>
+              <button
+              onClick={handleLogout}
+              
+              >
+                
+                logout
+                </button>
+              </Link></li> */}
+
+<>
+							{user?.uid ? (
+								<div className="text-white flex items-center gap-3">
+									<span className="hidden md:block">{user?.displayName}</span>
+									<button
+										onClick={handleLogout}
+										className="btn hidden sm:block  ">
+										Logout
+									</button>
+								</div>
+							) : (
+								<div className="text-white">
+									<Link to="/login"> Login </Link>
+								</div>
+							)}
+						</>
+
             <li><Link>About</Link></li>
             
           </ul>
